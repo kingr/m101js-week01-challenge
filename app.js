26acdf3a7dfb5dad3de0d6e4b97859e0678deb58
kingr <kurt_farao@yahoo.com>
2016-10-20 22:43:13 +0200

    complete challenge

--- app.js
+++ app.js
@@ -7,20 +7,33 @@ const express = require('express'),
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+// configure HTML engine
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
 const dbPath = 'mongodb://localhost:27017/video';
 
 MongoClient.connect(dbPath, function(err, db){
     assert.equal(null, err);
 
+    app.get('/', function(req, res, next){
+       res.render('home');
+    });
+
+    app.get('/movies', function(req, res, next){
+        db.collection('movies').find({}).toArray(function(err, doc){
+            console.log(doc);
+            res.render('movies', {'movies': doc});
+        });
+    });
     app.post('/addMovie', function(req, res, next){
-        console.log(req.body);
-        res.send('done');
+        const title = req.body.title;
+        const year = req.body.year;
+        const imdb = req.body.imdb;
+        db.collection('movies').insertOne({"title": title, "year": year, "imdb":imdb});
+        res.redirect('/movies');
     });
-
-    db.collection('movies').find({}).toArray(function(err, docs){
-       console.log(docs);
-    });
-
     var server = app.listen(3000, function(){
         var port = server.address().port;
         console.log('express running on port %s.', port);
