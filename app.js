d6be276a0ab1e7cdfe923742eaa0d1aec05d6d46
kingr <kurt_farao@yahoo.com>
2016-10-20 07:32:52 +0200

    add post route

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const express = require('express'),
     app = express(),
     engines = require('consolidate'),
     MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+    assert = require('assert'),
+    bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const dbPath = 'mongodb://localhost:27017/video';
 
 MongoClient.connect(dbPath, function(err, db){
     assert.equal(null, err);
 
-    console.log('connected to mongo!');
+    app.post('/addMovie', function(req, res, next){
+        console.log(req.body);
+        res.send('done');
+    });
 
-   db.close();
+    db.collection('movies').find({}).toArray(function(err, docs){
+       console.log(docs);
+    });
+
+    var server = app.listen(3000, function(){
+        var port = server.address().port;
+        console.log('express running on port %s.', port);
+    });
+
 });
